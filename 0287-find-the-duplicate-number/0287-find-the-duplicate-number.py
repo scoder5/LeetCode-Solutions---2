f9c6be4e7b6274e4624d5481089698fb78746443
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2022-12-04 07:51:41 +0530

    Time: 633 ms (96.32%), Space: 28 MB (36.17%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        #Floyd's Algorithm
         slow, fast = 0, 0
         while True:
             slow = nums[slow]
@@ -12,5 +11,5 @@ class Solution:
         while True:
             slow2 = nums[slow2]
             slow = nums[slow]
-            if slow2 == slow:
+            if slow == slow2:
                 return slow
